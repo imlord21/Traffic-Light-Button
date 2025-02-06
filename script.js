946c49fb594e7f1060f36ed8875fc569990b1075
imlord21 <noobulpro2@gmail.com>
2025-02-06 20:24:53 +0200

    Update 5.0

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const  MINI_SECONDS =10000;
-const BUTTON = document.getElementById("btn");
-const COLORS = ["red", "yellow", "green"];
+const button = document.getElementById("btn");
+const colors = ["red", "yellow", "green"];
 let colorIndex = 0;
 let intervalId;
 
 function changeColor() {
-    BUTTON.style.backgroundColor = COLORS[colorIndex];
-    colorIndex = (colorIndex + 1) % COLORS.length;
+    button.style.backgroundColor = colors[colorIndex];
+    colorIndex = (colorIndex + 1) % colors.length;
 }
 
 function toggleColor() {
